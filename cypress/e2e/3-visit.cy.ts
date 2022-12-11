2fd3d832944150281dcd91999e35cb91c3f289e8
Tomasz Stanisz <fremen1990@gmail.com>
2022-12-12 00:48:45 +0100

    Section 4 Visit, BaseURL Cypress Subject Management

--- cypress/e2e/3-visit.cy.ts
+++ cypress/e2e/3-visit.cy.ts
@@ -2,13 +2,27 @@ describe("basics", () => {
   beforeEach(() => {
     cy.visit("/textinput");
   });
+
   it("visit explanation - text input", () => {
     cy.log("Hello from it");
   });
+
   it("visit url and return it to log", () => {
     cy.url().then((url) => {
       cy.log("Hello from url: " + url);
+      expect(url).to.contain("/textinput");
     });
   });
+
+  it("title validation", () => {
+    cy.title().then((title) => {
+      cy.log("TITLE: " + title);
+      expect(title).to.be.equal("Text Input");
+    });
+  });
+
+  it("Input Challange", () => {
+    cy.get("input#newButtonName").type("TEST");
+    cy.get("button#updatingButton").click().should("have.text", "TEST");
+  });
 });
-// TODO  Section 4.  -  29. cy.url and BDD assertion
